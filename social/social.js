c53f642d2f6c07076e26f0611fd21a61303be2ee
salivity <94751817+salivity@users.noreply.github.com>
2026-09-09 20:10:02 +0100

    Update social.js

--- social/social.js
+++ social/social.js
@@ -1,3 +1,43 @@
 document.addEventListener("DOMContentLoaded", () => {
     console.info("Social Network Script Loaded");
+    
+    const social_links = [
+        {
+            "href":      "https://youtube.com/Salivity",
+            "title":     "YouTube",
+            "image":     `<svg viewBox="0 0 24 24"><path d="M10 15l5.19-3L10 9v6m11.56-7.83c.13.47.22 1.1.28 1.9.07.8.1 1.49.1 2.09L22 12c0 2.19-.16 3.8-.44 4.83-.25.9-.83 1.48-1.73 1.73-.47.13-1.33.22-2.65.28-1.3.07-2.49.1-3.59.1L12 19c-4.19 0-6.8-.16-7.83-.44-.9-.25-1.48-.83-1.73-1.73-.13-.47-.22-1.1-.28-1.9-.07-.8-.1-1.49-.1-2.09L2 12c0-2.19.16-3.8.44-4.83.25-.9.83-1.48 1.73-1.73.47-.13 1.33-.22 2.65-.28 1.3-.07 2.49-.1 3.59-.1L12 5c4.19 0 6.8.16 7.83.44.9.25 1.48.83 1.73 1.73z"></path></svg>`
+        },
+        {
+            "href":     "https://instagram.com/",
+            "title":    "Instagram",
+            "image":    `<svg viewBox="0 0 24 24"><path d="M7.8 2h8.4C19.4 2 22 4.6 22 7.8v8.4a5.8 5.8 0 0 1-5.8 5.8H7.8C4.6 22 2 19.4 2 16.2V7.8A5.8 5.8 0 0 1 7.8 2m-.2 2A3.6 3.6 0 0 0 4 7.6v8.8C4 18.39 5.61 20 7.6 20h8.8a3.6 3.6 0 0 0 3.6-3.6V7.6C20 5.61 18.39 4 16.4 4H7.6m9.65 1.5a1.25 1.25 0 0 1 1.25 1.25A1.25 1.25 0 0 1 17.25 8 1.25 1.25 0 0 1 16 6.75a1.25 1.25 0 0 1 1.25-1.25M12 7a5 5 0 0 1 5 5 5 5 0 0 1-5 5 5 5 0 0 1-5-5 5 5 0 0 1 5-5m0 2a3 3 0 0 0-3 3 3 3 0 0 0 3 3 3 3 0 0 0 3-3 3 3 0 0 0-3-3z"></path></svg>`
+        }
+    ];
+    
+    const socialLinksUnorderedLists = document.getElementsByClassName("social-links");
+    
+    for (const socialLinksUnorderedList of socialLinksUnorderedLists) {
+    
+        /*
+         * convert into image list
+         */
+         for(let i = 0; i < social_links.length; i++){
+         
+            const social_link = social_links[i];
+            
+            const listItem = document.createElement("li");
+            const anchor = document.createElement("a");
+            
+            anchor.setAttribute("href", social_link.href);
+            anchor.setAttribute("title", social_link.title);
+            anchor.innerHTML = social_link.image;
+            
+            listItem.appendChild(anchor)
+            socialLinksUnorderedList.appendChild(listItem);
+            
+         }
+         
+    }
+    
+    
 })
